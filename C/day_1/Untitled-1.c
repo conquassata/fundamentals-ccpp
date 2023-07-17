50c692012479aff3f39e862bdb6e4a8481509e78
Felipe <ainfanteh@unal.edu.co>
2023-07-17 01:21:43 -0500

    Add files via upload

---
+++ C/day_1/Untitled-1.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(){
+	printf("Then old age and experience, hand \n in hand,\nlead him to death and make him\n understand,\nafter a search so painful and so\n long,\nthat all his life he has been in the\n wrong.");
+    return 0;
+}
