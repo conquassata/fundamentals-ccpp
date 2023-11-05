3406529a8e8f69fae35ead47052246b00210c309
Felipe, 俳画 <ainfanteh@unal.edu.co>
2023-11-04 22:29:05 -0500

    Delete C directory

--- C/day_1/act_untitled-1.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){
-	printf("Then old age and experience, hand \n in hand,\nlead him to death and make him\n understand,\nafter a search so painful and so\n long,\nthat all his life he has been in the\n wrong.");
-    return 0;
-}
